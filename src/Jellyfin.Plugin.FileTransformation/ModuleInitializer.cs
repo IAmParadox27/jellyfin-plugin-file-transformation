0e8103a597ea9aeee42909f5b2a9d80f6111c6e8
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-08-23 20:53:32 +0100

    fix #13, #8: Added code to lookup jellyfin temp directory from IServerApplicationPaths and use that in place of just the default Path.GetTempFileName. Also added some logging into the module init code. Since we need to inject some variables also removed [ModuleInitializer] from the function and instead call it from the next earliest entrypoint PluginServiceRegistrator.

--- src/Jellyfin.Plugin.FileTransformation/ModuleInitializer.cs
+++ src/Jellyfin.Plugin.FileTransformation/ModuleInitializer.cs
@@ -2,6 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.Loader;
 using Jellyfin.Plugin.FileTransformation.Helpers;
+using MediaBrowser.Common.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Plugin.FileTransformation
 {
@@ -9,8 +11,7 @@ namespace Jellyfin.Plugin.FileTransformation
     {
         private static Dictionary<string, Assembly> s_dynamicAssemblies = new Dictionary<string, Assembly>();
         
-        [ModuleInitializer]
-        public static void Initialize()
+        public static void Initialize(IApplicationPaths? applicationPaths = null, ILogger? logger = null)
         {
             Assembly assembly = typeof(FileTransformationPlugin).Assembly;
             AssemblyLoadContext assemblyLoadContext = new AssemblyLoadContext("Jellyfin.Plugin.FileTransformation");
@@ -18,16 +19,26 @@ namespace Jellyfin.Plugin.FileTransformation
             
             foreach (string resource in resources.Where(x => x.EndsWith(".dll")))
             {
+                logger?.LogInformation($"Loading embedded dll: {Path.GetFileName(resource)}");
+                
                 using Stream? assemblyStream = assembly.GetManifestResourceStream(resource);
                 using MemoryStream memoryStream = new MemoryStream();
                 assemblyStream!.CopyTo(memoryStream);
                 assemblyStream.Position = 0;
                 
                 string tmpDllLocation = $"{Path.GetTempFileName()}.dll";
+
+                if (applicationPaths != null)
+                {
+                    tmpDllLocation = Path.Combine(applicationPaths.TempDirectory, Path.GetFileName(tmpDllLocation));
+                }
                 
+                logger?.LogInformation($"Writing dll to: {tmpDllLocation} to extract AssemblyName details, will be removed after loading");
                 File.WriteAllBytes(tmpDllLocation, memoryStream.ToArray());
                 
                 AssemblyName assemblyName = AssemblyName.GetAssemblyName(tmpDllLocation);
+                
+                logger?.LogInformation($"Deleting: {tmpDllLocation}");
                 File.Delete(tmpDllLocation);
                 
                 Assembly loadedAssembly;
@@ -40,6 +51,7 @@ namespace Jellyfin.Plugin.FileTransformation
                     loadedAssembly = assemblyLoadContext.Assemblies.First(x => x.FullName == assemblyName.FullName);
                 }
                 
+                logger?.LogInformation($"Loaded assembly: {loadedAssembly.FullName}");
                 s_dynamicAssemblies.Add(loadedAssembly.FullName!, loadedAssembly);
             }
 
