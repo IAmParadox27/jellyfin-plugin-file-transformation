b876351e7024d198504452fdfde102af69a2f9ae
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-10-27 22:42:03 +0000

    Update File Transformation to 10.11.1

--- src/Jellyfin.Plugin.FileTransformation/JellyfinVersionSpecific/10.11/StartupHelper_VersionSpecific.cs
+++ src/Jellyfin.Plugin.FileTransformation/JellyfinVersionSpecific/10.11/StartupHelper_VersionSpecific.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Jellyfin.Plugin.FileTransformation.Library;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Primitives;
 
 namespace Jellyfin.Plugin.FileTransformation.JellyfinVersionSpecific
