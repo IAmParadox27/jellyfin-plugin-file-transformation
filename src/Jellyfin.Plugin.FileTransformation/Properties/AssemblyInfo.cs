c24e02a35863c0dc1fc2c9d40b292008776ac24f
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-11-03 22:24:53 +0000

    Updated plugin to support 10.11.2

--- src/Jellyfin.Plugin.FileTransformation/Properties/AssemblyInfo.cs
+++ src/Jellyfin.Plugin.FileTransformation/Properties/AssemblyInfo.cs
@@ -9,4 +9,4 @@ using Jellyfin.Plugin.FileTransformation.Attributes;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyTitle("Jellyfin.Plugin.FileTransformation")]
 [assembly: AssemblyVersion("2.4.0.0")]
-[assembly: JellyfinVersion("10.11.0")]
+[assembly: JellyfinVersion("10.11.2")]
