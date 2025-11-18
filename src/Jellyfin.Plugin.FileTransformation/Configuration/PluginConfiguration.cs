a1c757efcf5f8ed0ec29ef484ea2699b7871477a
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-11-18 01:12:03 +0000

    Added support for this plugin to define transformations via its configuration. Completely untested at this stage

--- src/Jellyfin.Plugin.FileTransformation/Configuration/PluginConfiguration.cs
+++ src/Jellyfin.Plugin.FileTransformation/Configuration/PluginConfiguration.cs
@@ -11,5 +11,18 @@ namespace Jellyfin.Plugin.FileTransformation.Configuration
     public class PluginConfiguration : BasePluginConfiguration
     {
         public DebugLoggingState DebugLoggingState { get; set; } = DebugLoggingState.Disabled;
+        
+        public PluginDefinedTransformation[] Transformations { get; set; } = Array.Empty<PluginDefinedTransformation>();
+    }
+
+    public class PluginDefinedTransformation
+    {
+        public Guid Id { get; set; }
+        
+        public string FilenamePattern { get; set; } = string.Empty;
+        
+        public string SearchText { get; set; } = string.Empty;
+        
+        public string ReplaceText { get; set; } = string.Empty;
     }
 }
--- src/Jellyfin.Plugin.FileTransformation/FileTransformationPlugin.cs
+++ src/Jellyfin.Plugin.FileTransformation/FileTransformationPlugin.cs
@@ -1,8 +1,10 @@
 ﻿using Jellyfin.Plugin.FileTransformation.Configuration;
+using Jellyfin.Plugin.FileTransformation.Library;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Jellyfin.Plugin.FileTransformation
 {
@@ -16,11 +18,65 @@ namespace Jellyfin.Plugin.FileTransformation
         
         public IServiceProvider ServiceProvider { get; }
         
-        public FileTransformationPlugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer, IServiceProvider serviceProvider) : base(applicationPaths, xmlSerializer)
+        public FileTransformationPlugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer, IServiceProvider serviceProvider, IWebFileTransformationWriteService writeService) : base(applicationPaths, xmlSerializer)
         {
             Instance = this;
             
             ServiceProvider = serviceProvider;
+
+            foreach (PluginDefinedTransformation transformation in Configuration.Transformations)
+            {
+                writeService.AddTransformation(transformation.Id, transformation.FilenamePattern, (path, contents) => HandlePluginConfigTransformation(path, contents, transformation));
+            }
+        }
+
+        public override void UpdateConfiguration(BasePluginConfiguration configuration)
+        {
+            PluginDefinedTransformation[] previousTransforms = Configuration.Transformations;
+            
+            base.UpdateConfiguration(configuration);
+            
+            if (configuration is PluginConfiguration newConfig)
+            {
+                // Once the configuration has been updated we need to remove any that have been removed, add ones that have been added and update everything else
+                foreach (PluginDefinedTransformation previousTransform in previousTransforms)
+                {
+                    if (!newConfig.Transformations.Any(x => x.Id == previousTransform.Id))
+                    {
+                        ServiceProvider.GetRequiredService<IWebFileTransformationWriteService>().RemoveTransformation(previousTransform.Id);
+                    }
+                }
+
+                foreach (PluginDefinedTransformation newTransform in newConfig.Transformations)
+                {
+                    if (previousTransforms.Any(x => x.Id == newTransform.Id))
+                    {
+                        ServiceProvider.GetRequiredService<IWebFileTransformationWriteService>().UpdateTransformation(newTransform.Id, newTransform.FilenamePattern, (path, contents) => HandlePluginConfigTransformation(path, contents, newTransform));
+                    }
+                    else
+                    {
+                        ServiceProvider.GetRequiredService<IWebFileTransformationWriteService>().AddTransformation(newTransform.Id, newTransform.FilenamePattern, (path, contents) => HandlePluginConfigTransformation(path, contents, newTransform));
+                    }
+                }
+            }
+        }
+
+        private async Task HandlePluginConfigTransformation(string path, Stream contents, PluginDefinedTransformation transformation)
+        {
+            if (string.IsNullOrWhiteSpace(transformation.ReplaceText) || string.IsNullOrWhiteSpace(transformation.SearchText))
+            {
+                return;
+            }
+                    
+            using StreamReader reader = new StreamReader(contents, leaveOpen: true);
+            string currentContent = await reader.ReadToEndAsync();
+                    
+            contents.Seek(0, SeekOrigin.Begin);
+
+            string transformedString = currentContent.Replace(transformation.ReplaceText, transformation.SearchText);
+                    
+            using StreamWriter textWriter = new StreamWriter(contents, null, -1, true);
+            textWriter.Write(transformedString);
         }
         
         public IEnumerable<PluginPageInfo> GetPages()
